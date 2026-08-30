a15073ec837c678c3630ccc3be9c3d578af1db2d
Melquizedeque1 <imelquizedequesa@gmail.com>
2026-08-30 02:07:54 -0400

    feat: adiciona exercicio de tipos primitivos (String, int, double, float)

---
+++ src/fundamentos/variaveis/TiposPrimitivos.java
@@ -0,0 +1,25 @@
+package fundamentos.variaveis;
+
+public class TiposPrimitivos {
+
+    public static void main(String[] args){
+        String nome = "Melquizedeque";
+        int idade = 20;
+        double peso = 92.20;
+        float altura = 1.73f;
+
+
+        IO.println("Me chamo " + nome + ", tenho " + idade + " anos e peso " + peso + " quilos de puro musculo kkkkk.\nTenho " + altura + " de altura.");
+
+        // Outros tipos primitivos que existem em Java, mas não usei aqui:
+        // boolean -> verdadeiro ou falso (true/false)
+        // char -> um único caractere ('M')
+        // long -> números inteiros bem grandes
+        // short -> números inteiros pequenos
+        // byte    -> números inteiros bem pequenos (-128 a 127)
+
+
+    }
+}
+
+
