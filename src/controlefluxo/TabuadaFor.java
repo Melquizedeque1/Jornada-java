32ea19b31b7e199186ece0948ee55a2760d26ed6
Melquizedeque1 <imelquizedequesa@gmail.com>
2026-09-01 21:30:39 -0400

    feat: adiciona tabuada usando for

--- src/controlefluxo/TabuadaFor.java
+++ src/controlefluxo/TabuadaFor.java
@@ -1,4 +1,13 @@
 package controlefluxo;
 
 public class TabuadaFor {
+
+    public static void main(String[] args){
+
+        int numero = 7;
+
+        for (int i = 1; i <= 10; i++){
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+    }
 }
